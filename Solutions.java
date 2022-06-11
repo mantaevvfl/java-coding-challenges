5b2f6afb97b31040795e16d1b7d467e961d9620f
Ali Mantaev <mantaev.vfl@gmail.com>
2022-06-11 19:30:17 -0400

    06-11-22

--- Solutions.java
+++ Solutions.java
@@ -29,3 +29,14 @@ public static int lonelyinteger(List<Integer> a) {
     }
     return doubledSum - sum;
 }
+
+// Given a square matrix, return the absolute difference between the sum of its diagonals.
+public static int diagonalDifference(List<List<Integer>> arr) {
+    int n = arr.size();
+    int leftDiagonal = 0, rightDiagonal = 0;
+    for (int i = 0; i < n; i++) {
+        leftDiagonal += arr.get(i).get(i);
+        rightDiagonal += arr.get(i).get(n-1-i);
+    }
+    return Math.abs(rightDiagonal - leftDiagonal);
+}
